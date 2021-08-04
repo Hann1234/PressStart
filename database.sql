1296a4cccecdb2295d65d849ee2c6f954603504b
Hann1234 <christopherjhannigan@gmail.com>
2021-08-04 11:07:55 -0500

    revised database and added table info into database.sql

--- database.sql
+++ database.sql
@@ -3,8 +3,44 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
-);
+    "password" VARCHAR (1000) NOT NULL,
+    "profile_image" VARCHAR (1000),
+    "profile_description" TEXT,
+    "user_play_style" VARCHAR (80),
+    "user_fav_game_1" VARCHAR (80),
+    "user_fav_game_2" VARCHAR (80),
+    "user_fav_game_3" VARCHAR (80),
+    "game_1_rank" VARCHAR (80),
+    "game_2_rank" VARCHAR (80),
+    "game_3_rank" VARCHAR (80),
+    "discord_link" VARCHAR (80),
+    "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+
+CREATE TABLE "games" (
+	"id" SERIAL PRIMARY KEY,
+	"game_title" VARCHAR (80),
+	"game_cover" VARCHAR (1000),
+	"game_description" TEXT
+	);
+	
+CREATE TABLE "user_game" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INT REFERENCES "user" ON DELETE CASCADE NOT NULL,
+	"game_id" INT REFERENCES "games" ON DELETE CASCADE NOT NULL,
+	"time_start" TIMESTAMP NOT NULL,
+	"time_end" TIMESTAMP,
+	"created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	);
+	
+CREATE TABLE "matches" (
+	"id" SERIAL PRIMARY KEY,
+	"primary_user_id" INT REFERENCES "user" ON DELETE CASCADE NOT NULL,
+	"secondary_user_id" INT REFERENCES "user" ON DELETE CASCADE NOT NULL,
+	"invite_status" VARCHAR (80) DEFAULT 'pending',
+	"created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	);
