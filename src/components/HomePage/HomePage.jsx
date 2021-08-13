3e179cbd1b2b1a35cae4864f850e43de65906fd4
Hann1234 <christopherjhannigan@gmail.com>
2021-08-12 21:53:10 -0500

    revised buttons

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -3,20 +3,35 @@ import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
+//material.ui button imports:
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles({
+  button: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  },
+});
 
 function HomePage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   const history = useHistory();
+  const classes = useStyles();
 
   return (
     <div className="container">
       <h2>Hello there, {user.username}! Press START to find your squad!</h2>
-      <p>Your ID is: {user.id}</p>
-      <button className="btn" onClick={ () => history.push('/gameselect')}>START</button>
-      <button className="btn" onClick={ () => history.push('/profile')}>PROFILE</button>
-      <button className="btn" onClick={ () => history.push('/squadlist')}>SQUAD</button>
-      <LogOutButton className="btn" />
+      <Button className={classes.button} onClick={ () => history.push('/gameselect')}>START</Button>
+      <Button className={classes.button} onClick={ () => history.push('/profile')}>PROFILE</Button>
+      <Button className={classes.button} onClick={ () => history.push('/squadlist')}>SQUAD</Button>
+      <LogOutButton className={classes.button} />
     </div>
   );
 }
