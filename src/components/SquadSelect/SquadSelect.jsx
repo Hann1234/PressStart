10ef8906689d18e7852c87754e9cbb9e2836534b
Hann1234 <christopherjhannigan@gmail.com>
2021-09-14 15:45:56 -0500

    cleaned code, removed old notes

--- src/components/SquadSelect/SquadSelect.jsx
+++ src/components/SquadSelect/SquadSelect.jsx
@@ -111,16 +111,6 @@ function SquadSelect() {
         });
     }
 
-    // const displayDate = (date) => {
-    //     return (
-    //         <span>
-    //         {date.toLocaleString("en-US", {dateStyle: "short", timeStyle: "short", hour12: true})}
-    //         </span>
-    //         );
-    // }
-    
-    // console.log('displayDate', displayDate(new Date()));
-
     return (
         <center>
         <style>{games.length === 0 ? "body {background-color: black;}" : //allows the user to reload the page
@@ -199,58 +189,3 @@ function SquadSelect() {
 }
 
 export default SquadSelect;
-
-// function SquadSelect() {
-//     // this component is the squad selection screen for the user to invite other users to their squad.
-//     const otherUsers = useSelector((store) => store.userGame);
-//     const history = useHistory();
-//     const dispatch = useDispatch();
-//     const params = useParams();
-
-//     console.log('otherUsers', otherUsers);
-
-//     useEffect(() => {
-//         dispatch({ 
-//             type: 'FETCH_USERS',
-//             payload: {
-//                 game_id: params.id
-//             } 
-//         });
-//     }, [params.id]); //need to include [params.id] here?
-
-//     const handleClick = (usersID, time) => {
-
-//         console.log('inside post dispatch', usersID);
-//         // event.preventDefault(); do i need to include this?
-
-//         dispatch({ 
-//             type: 'CREATE_INVITE',
-//             payload: {
-//                 secondary_user_id: usersID,
-//                 matched_game_id: params.id,
-//                 matched_time: time
-//             } 
-//         });
-//     }
-    
-//     return (
-//     <div className="container">
-//         <h1>SELECT YOUR SQUAD</h1>
-//         <button className="btn" onClick={() => history.push('/')}>RETURN HOME</button>
-//         <section className="users">
-//             {otherUsers.map(users => {
-//                 return (
-//                     <div key={users.id} >
-//                         <h2>{users.username}</h2>
-//                         <h3>{users.time_start}</h3>
-//                         <img src={users.profile_image} alt={users.username}/>
-//                         <p>{users.profile_description}</p>
-//                         <p>{users.user_play_style}</p>
-//                         <button className="btn" onClick={() => handleClick(users.id, users.time_start)}>ADD TO SQUAD</button>
-//                     </div>
-//                 );
-//             })}
-//         </section>
-//     </div>
-//     );
-// }
